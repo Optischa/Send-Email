7ea1d1559123d9fb0fd92c906e55daf7e3a3bf33
Optischa <59180449+Optischa@users.noreply.github.com>
2021-10-07 11:59:04 +0200

    Update sender

--- src/main/java/de/optischa/mail/Sender.java
+++ src/main/java/de/optischa/mail/Sender.java
@@ -10,6 +10,7 @@ import javax.mail.internet.MimeMultipart;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,26 +18,50 @@ public class Sender {
 
     public static void sendEmail(String fromEmail, String toEmail, String title, String body, Session session) {
         Message message = new MimeMessage(session);
-        sendMessage(message, body, title, toEmail, fromEmail);
+        sendMessage(message, body, title, toEmail, fromEmail, null);
+    }
+
+    public static void sendEmail(String fromEmail, String fromName,String toEmail, String title, String body, Session session) {
+        Message message = new MimeMessage(session);
+        sendMessage(message, body, title, toEmail, fromEmail, fromName);
     }
 
     public static void sendArrayEmail(String fromEmail, List<String> toEmails, String title, String body, Session session) {
         for (String toEmail : toEmails) {
             Message message = new MimeMessage(session);
-            sendMessage(message, body, title, toEmail, fromEmail);
+            sendMessage(message, body, title, toEmail, fromEmail, null);
+        }
+    }
+
+    public static void sendArrayEmail(String fromEmail, String fromName,List<String> toEmails, String title, String body, Session session) {
+        for (String toEmail : toEmails) {
+            Message message = new MimeMessage(session);
+            sendMessage(message, body, title, toEmail, fromEmail, fromName);
         }
     }
 
     public static void sendArrayEmail(String fromEmail, List<String> toEmails, String title, List<Component> components, Session session) {
         for (String toEmail : toEmails) {
             Message message = new MimeMessage(session);
-            sendMessage(message, getBodyFromComponent(components), title, toEmail, fromEmail);
+            sendMessage(message, getBodyFromComponent(components), title, toEmail, fromEmail, null);
+        }
+    }
+
+    public static void sendArrayEmail(String fromEmail, String fromName, List<String> toEmails, String title, List<Component> components, Session session) {
+        for (String toEmail : toEmails) {
+            Message message = new MimeMessage(session);
+            sendMessage(message, getBodyFromComponent(components), title, toEmail, fromEmail, fromName);
         }
     }
 
     public static void sendEmail(String fromEmail, String toEmail, String title, List<Component> components, Session session) {
         Message message = new MimeMessage(session);
-        sendMessage(message, getBodyFromComponent(components), title, toEmail, fromEmail);
+        sendMessage(message, getBodyFromComponent(components), title, toEmail, fromEmail, null);
+    }
+
+    public static void sendEmail(String fromEmail, String fromName, String toEmail, String title, List<Component> components, Session session) {
+        Message message = new MimeMessage(session);
+        sendMessage(message, getBodyFromComponent(components), title, toEmail, fromEmail, fromName);
     }
 
     private static String getBodyFromComponent(List<Component> components) {
@@ -55,9 +80,11 @@ public class Sender {
         return stringBuilder.toString();
     }
 
-    private static void sendMessage(Message message, String body, String title, String toEmail, String fromEmail) {
+    private static void sendMessage(Message message, String body, String title, String toEmail, String fromEmail, String fromName) {
         try {
-            message.setFrom(new InternetAddress(fromEmail));
+            if(fromName != null) {
+                message.setFrom(new InternetAddress(fromEmail, fromName));
+            }
             message.setRecipients(
                     Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject(title);
@@ -70,7 +97,7 @@ public class Sender {
 
             message.setContent(multipart);
             Transport.send(message);
-        } catch (MessagingException e) {
+        } catch (MessagingException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
